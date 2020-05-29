d40b61df5a9cfc79f422a4987ed58d15bdc59f87
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-05-29 12:10:34 -0600

    Add query selectors and event listeners for the activity buttons.

---
+++ main.js
@@ -0,0 +1,7 @@
+var studyButton = document.querySelector('.study-button');
+var meditateButton = document.querySelector('.meditate-button');
+var exerciseButton = document.querySelector('.exercise-button')
+
+studyButton.addEventListener('click', changeColorOfStudyButton);
+meditateButton.addEventListener('click', changeColorOfMeditateButton);
+exerciseButton.addEventListener('click', changeColorOfExerciseButton);
