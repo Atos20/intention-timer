4031282855c1014ee7a1bf34b818d8ff3e97c566
Katy St Sauveur <katyrogowski@yahoo.com>
2020-05-29 13:31:09 -0700

    Add color highlight on button click

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 var studyButton = document.querySelector('.study-button');
 var meditateButton = document.querySelector('.meditate-button');
-var exerciseButton = document.querySelector('.exercise-button')
+var exerciseButton = document.querySelector('.exercise-button');
+
 
 studyButton.addEventListener('click', changeColorOfStudyButton);
 meditateButton.addEventListener('click', changeColorOfMeditateButton);
 exerciseButton.addEventListener('click', changeColorOfExerciseButton);
 
 function changeColorOfStudyButton() {
-  studyButton.classList.add('hide');
+  studyButton.classList.toggle('green');
+  studyButton.classList.toggle('white');
+}
+
+function changeColorOfMeditateButton() {
+  meditateButton.classList.toggle('purple');
+  meditateButton.classList.toggle('white');
+}
+
+function changeColorOfExerciseButton() {
+  exerciseButton.classList.toggle('red');
+  exerciseButton.classList.toggle('white');
 }
