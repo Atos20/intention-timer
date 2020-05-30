72d9ab1e055a482d262ce1de09da0086769365fb
Orlando Murcio <ato.murcio@gmail.com>
2020-05-29 20:33:43 -0600

    set only numbers function

--- main.js
+++ main.js
@@ -6,12 +6,37 @@ var studyIconActive = document.querySelector('.study-icon-active');
 var meditateIcon = document.querySelector('.meditate-icon');
 var meditateIconActive = document.querySelector('.meditate-icon-active');
 var exerciseIcon = document.querySelector('.exercise-icon');
-var exerciseIconActive = document.querySelector('.exercise-icon-active')
-
+var exerciseIconActive = document.querySelector('.exercise-icon-active');
+var minutesNumberOnly = document.querySelector('.minutes-input');
+var secondsNumberOnly = document.querySelector('.seconds-input');
+var startActivityButton = document.querySelector('.start-activity-button')
 
 studyButton.addEventListener('click', changeColorOfStudyButton);
 meditateButton.addEventListener('click', changeColorOfMeditateButton);
 exerciseButton.addEventListener('click', changeColorOfExerciseButton);
+// minuteNumberOnly.addEventListener('keypress', onlyNumbers);
+
+// function onlyNumbers(event) {
+//   var key = event.which;
+//   if((key < 48 || key > 57)) {
+//       event.preventDefault();
+//     }
+// }
+minutesNumberOnly.addEventListener('keypress', function(event) {
+  // minuteNumberOnly.value
+  var key1 = event.which;
+  if((key1 < 48 || key1 > 57)) {
+    event.preventDefault();
+  }
+})
+
+secondsNumberOnly.addEventListener('keypress', function(event) {
+  var key2 = event.which;
+  if((key2 < 48 || key2 > 57)) {
+    event.preventDefault();
+  }
+})
+
 
 function changeColorOfStudyButton() {
   studyButton.classList.toggle('green');
