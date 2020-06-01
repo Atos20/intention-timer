afff15cfbff592dcf61ba8c6226f254a98be3d5b
Orlando Murcio <ato.murcio@gmail.com>
2020-05-31 20:31:08 -0600

    disabled timer button and display congratulatory message

--- main.js
+++ main.js
@@ -114,12 +114,12 @@ function timerStart() {
     minutesText.innerText = Math.floor( (allSeconds/60) % 60 )
     secondsText.innerText = Math.floor( (allSeconds) % 60 );
     if (allSeconds < 0) {
-      secondsText.innerText = `00`;
+      secondsText.innerText = `0`;
       minutesText.innerText = `0`
       clearInterval(intentionTimer);
       alert("hello");
-      minutesText.innerText = `WELL`
-      secondsText.innerText = `DONE`;
+      timerButton.innerText = `WELL-DONE`
+      timerButton.disabled = true;
     }
     if (secondsText.innerText < 10) {
       secondsText.innerText = ('0' + secondsText.innerText);
